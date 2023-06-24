9f7d13cb670a201c6384136db4c82fc6ede9068f
Jeremy Gillick <j_gillick@yahoo.com>
2023-06-24 00:31:19 -0700

    Update jest config

--- packages/ingestion/jest.config.js
+++ packages/ingestion/jest.config.js
@@ -1,16 +1,15 @@
 module.exports = {
   preset: "ts-jest",
+  rootDir: "./src",
   clearMocks: true,
   resetMocks: true,
   collectCoverage: false,
   coveragePathIgnorePatterns: ["/node_modules/"],
   coverageProvider: "v8",
+  coverageDirectory: "../coverage",
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   maxWorkers: "50%",
-
-  // The root directory that Jest should scan for tests and modules within
-  rootDir: "./src",
 
   // The glob patterns Jest uses to detect test files
   testMatch: ["**/?(*.)+(test).[tj]s?(x)"],
