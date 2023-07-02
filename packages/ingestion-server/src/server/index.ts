e5bc18026159ee11c3d356d6b061cc5e07d8d260
Jeremy <j_gillick@yahoo.com>
2023-07-01 18:11:25 -0700

    Add default route

--- packages/ingestion-server/src/server/index.ts
+++ packages/ingestion-server/src/server/index.ts
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { Response, Request } from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import { eventRoutes } from "./routes/event";
 import { userRoutes } from "./routes/user";
+import { version } from "../../package.json";
 
 const PORT = process.env.PORT ?? 6433;
 const app = express();
@@ -13,6 +14,11 @@ app.use(morgan("tiny"));
 // JSON body middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Default route
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send(`Greenhouse Server: ${version}`);
+});
 
 // Routes
 eventRoutes(app);
